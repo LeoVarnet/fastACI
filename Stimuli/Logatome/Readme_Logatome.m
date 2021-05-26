690380269731c401e1b4dcc8cb12b35eaa719216
alejandro-ens <alejandro.osses@ens.psl.eu>
2021-05-26 16:35:40 +0200

    speechACI_Logatome: adding speech samples for ab_ba/ad_da comparison

---
+++ Stimuli/Logatome/Readme_Logatome.m
@@ -0,0 +1,9 @@
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+fprintf('%s.m:\n',upper(mfilename))
+fprintf('\tSpeech sounds from the Logatome corpus.\n')
+fprintf('\tThe files here have been edited by Leo Varnet\n')
+fprintf('\t(e.g., level adjusted, level balanced across phonemes).\n')
+fprintf('\tThe following files are used:\n')
+fprintf('\t\tap_pa, at_ta from  Male  French speaker S46M (sounds L008_V6_M1_N2, L002_V6_M1_N2)\n') % As-received-20210409, Slack
+fprintf('\t\tab_ba, ad_da from Female French speaker S41F (sounds L007_V1_M1_N1, L001_V1_M1_N1)\n') % As-received-20210526, email
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
