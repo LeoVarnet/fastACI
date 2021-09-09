043038b703ed331284928db6522cff951b138aae
alejandro-ens <alejandro.osses@ens.psl.eu>
2021-09-09 17:32:48 +0200

    small update to publ_osses2021c_DAGA_0_checkdata.m

--- Publications/publ_osses2021c_DAGA_0_checkdata.m
+++ Publications/publ_osses2021c_DAGA_0_checkdata.m
@@ -6,20 +6,26 @@ function bAre_stored_locally = publ_osses2021c_DAGA_0_checkdata
 
 dir_where = fastACI_dir_data;
 experiment = 'speechACI_varnet2013';
-dirs2check = {[experiment filesep 'osses2021c_S01' filesep], ...
-              [experiment filesep 'osses2021c_S02' filesep]};
+
+dir_where = [dir_where experiment filesep];
+dirs2check = {'osses2021c_S01', ...
+              'osses2021c_S02'};
 
 bAre_stored_locally = ones(size(dirs2check));
 
 for i = 1:length(dirs2check)
     
-    dir2check = [dir_where dirs2check{i}];
+    dir2check = [dir_where dirs2check{i} filesep];
     if ~exist(dir2check,'dir')
         fprintf('%s: directory %s successfully not found on disk\n',upper(mfilename),dir2check);
         fprintf('\t Next steps to perform: \n');
         fprintf('\t 1. Go to https://doi.org/10.5281/zenodo.5483835 (reference osses2021c_data) and download the two zip files\n');
         fprintf('\t 2. Uncompress the zip files and locate them into %s\n',dir_where);
         fprintf('\t 3. Re-run the current script until you don''t get errors \n');
+        
+        if ~exist(dir_where,'dir')
+            mkdir(dir_where)
+        end
         
         bAre_stored_locally = 0;
         return;
