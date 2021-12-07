03a288df92b6e43d7b54e57c9440d8270fe50525
Leo Varnet <leo.varnet@ens.psl.eu>
2021-12-07 11:20:05 +0100

    Update msg_currenttrial.m

--- Interface/msg_currenttrial.m
+++ Interface/msg_currenttrial.m
@@ -20,8 +20,15 @@ switch cfg_game.Language
         fprintf('\n    *** EXP\311RIENCE PRINCIPALE ***\n\n');
         fprintf('    \311coute num\351ro %.0f sur %.0f (prochaine pause dans %.0f \351coutes)\n',i_current,cfg_game.N,N_for_next_stop);
         fprintf('    Volume relatif de la voix : %.2f dB\n',expvar);
-        if i_current>=100
-            fprintf('    Pourcentage de r\351ponses "%s" : %.1f %%\n',cfg_game.response_names{1},100*sum(data_passation.n_responses(end-100+1:end)==1)/100);
+        if i_current>100
+            bias_r1 = 100*sum(data_passation.n_responses(end-100+1:end)==1)/100;
+            if bias_r1>60
+                fprintf('    Pourcentage de r\351ponses "%s" = %.0f %% (trop de "%s") \n',cfg_game.response_names{1},bias_r1,cfg_game.response_names{1});
+            elseif bias_r1<40
+                fprintf('    Pourcentage de r\351ponses "%s" = %.0f %% (trop de "%s") \n',cfg_game.response_names{1},bias_r1,cfg_game.response_names{2});
+            else
+                fprintf('    Pourcentage de r\351ponses "%s" normal \n',cfg_game.response_names{1});
+            end
         end
         fprintf('\n');
 end
