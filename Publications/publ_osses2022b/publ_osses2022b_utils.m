d3310c12433392bdf0ffa6fb595b209003a5eec7
alejandro-ens <alejandro.osses@ens.psl.eu>
2022-01-27 02:31:50 +0100

    Get_savenames.m: fix to avoid errors for empty folders

--- Publications/publ_osses2022b/publ_osses2022b_utils.m
+++ Publications/publ_osses2022b/publ_osses2022b_utils.m
@@ -1,4 +1,4 @@
-function fpubl_osses2022b_utils
+function publ_osses2022b_utils
 % function publ_osses2022b_utils
 %
 % Author: Alejandro Osses
@@ -61,8 +61,9 @@ switch type_action
                     info1 = dir(Cond_name2store);
                     info2 = dir(name_dst);
 
-                    fprintf('Trying to overwrite a new file dated: %s\n',info1.date);
-                    fprintf('  by another dated : %s\n',info2.date);
+                    fprintf('%s:\n',load_name);
+                    fprintf('\tTrying to overwrite a new file dated: %s\n',info1.date);
+                    fprintf('\tby another dated : %s\n',info2.date);
                     bOverwrite = input('Press 1 to proceed with the overwritting or 0 to not to: ');
                     if bOverwrite
                         copyfile(Cond_name2store,name_dst);
@@ -81,55 +82,59 @@ switch type_action
         disp('')
         
     case 'Copy_results_from_Dropbox'
-        % N_copied = 0;
+        N_copied = 0;
          
         dir_subj_dropbox = [dir_dropbox experiment filesep Subject_ID filesep];
         % if ~exist(dir_subj_dropbox,'dir')
         %     mkdir(dir_subj_dropbox);
         % end
-        % dir_results = [fastACI_dir_data experiment filesep Subject_ID filesep 'Results' filesep];
+        dir_results = [fastACI_dir_data experiment filesep Subject_ID filesep 'Results' filesep];
         % 
-        % for i = 1:length(Conditions)
-        %     exp2filter = ['save*' Conditions{i} '.mat'];
-        %     [load_name_full, load_name] = Get_savenames(dir_results, exp2filter);
-        % 
-        %     name_dst = [dir_subj_dropbox load_name];
-        %     if ~exist(name_dst,'file')
-        %         copyfile(load_name_full,name_dst);
-        %         fprintf('%s successfully copied to %s\n',load_name,dir_subj_dropbox);
-        %         N_copied = N_copied + 1;
-        %     else
-        %         fprintf('%s is already located in %s\n',load_name,dir_subj_dropbox);
-        %         fprintf('(file not being copied)\n');
-        %     end
-        % end
-        % 
-        % if N_copied ~= 0
-        %     % Then we need to copy the Conditions_file too...
-        %     load_name = ['Conditions-for-' Subject_ID '+' experiment '.mat'];
-        %     Cond_name2store = [fastACI_paths('dir_output') load_name];
-        %     if exist(Cond_name2store,'file')
-        %         name_dst = [dir_subj_dropbox load_name];
-        %         if exist(name_dst,'file')
-        %             info1 = dir(Cond_name2store);
-        %             info2 = dir(name_dst);
-        % 
-        %             fprintf('Trying to overwrite a new file dated: %s\n',info1.date);
-        %             fprintf('  by another dated : %s\n',info2.date);
-        %             bOverwrite = input('Press 1 to proceed with the overwritting or 0 to not to: ');
-        %             if bOverwrite
-        %                 copyfile(Cond_name2store,name_dst);
-        %                 fprintf('%s was successfully copied to %s\n',load_name,dir_subj_dropbox);
-        %             else
-        %                 fprintf('The file %s was NOT overwritten\n',load_name);
-        %             end
-        %         else
-        %             % No file in destination, so no problem to directly
-        %             %   copy the Cond_name2store file:
-        %             copyfile(Cond_name2store,name_dst);
-        %             fprintf('%s was successfully copied to %s\n',load_name,dir_subj_dropbox);
-        %         end
-        %     end
-        % end
+        for i = 1:length(Conditions)
+            exp2filter = ['save*' Conditions{i} '.mat'];
+            [load_name_full, load_name] = Get_savenames(dir_subj_dropbox, exp2filter);
+        
+            name_dst = [dir_results load_name];
+            if ~exist(name_dst,'file')
+                copyfile(load_name_full,name_dst);
+                fprintf('%s successfully copied to %s\n',load_name,dir_subj_dropbox);
+                N_copied = N_copied + 1;
+            else
+                fprintf('%s is already located in %s\n',load_name,dir_subj_dropbox);
+                fprintf('(file not being copied)\n');
+            end
+        end
+         
+        if N_copied ~= 0
+            % Then we need to copy the Conditions_file too...
+            load_name = ['Conditions-for-' Subject_ID '+' experiment '.mat'];
+            Cond_name2store = [dir_subj_dropbox load_name]; % [fastACI_paths('dir_output') load_name];
+            if exist(Cond_name2store,'file')
+                name_dst = [fastACI_paths('dir_output') load_name];
+                if exist(name_dst,'file')
+                    info1 = dir(Cond_name2store);
+                    info2 = dir(name_dst);
+         
+                    fprintf('Trying to overwrite a new file dated: %s\n',info1.date);
+                    fprintf('  by another dated : %s\n',info2.date);
+                    if strcmp(info1.date,info1.date)
+                        fprintf('The file %s is already up to date...\n',load_name);
+                    else
+                        bOverwrite = input('Press 1 to proceed with the overwritting or 0 to not to: ');
+                        if bOverwrite
+                            copyfile(Cond_name2store,name_dst);
+                            fprintf('%s was successfully copied to %s\n',load_name,dir_subj_dropbox);
+                        else
+                            fprintf('The file %s was NOT overwritten\n',load_name);
+                        end
+                    end
+                else
+                    % No file in destination, so no problem to directly
+                    %   copy the Cond_name2store file:
+                    copyfile(Cond_name2store,name_dst);
+                    fprintf('%s was successfully copied to %s\n',load_name,dir_subj_dropbox);
+                end
+            end
+        end
         % disp('')        
 end
--- Utility/Get_savenames.m
+++ Utility/Get_savenames.m
@@ -4,6 +4,8 @@ function [load_name_full,load_name] = Get_savenames(dir_results, exp2filter, dir
 % Programmed by Alejandro Osses
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+load_name = []; % Initialisation
+load_name_full = []; % Initialisation
 ListSavegame = dir([dir_results exp2filter]);
 index_savegame = 0;
 bytes = 0;
@@ -15,17 +17,19 @@ for j=index_all
         bytes = ListSavegame(j).bytes; % looks for the largest MAT file
     end
 end
-load_name      = ListSavegame(index_savegame).name;
-load_name_full = [dir_results load_name];
+if ~isempty(ListSavegame)
+    load_name      = ListSavegame(index_savegame).name;
+    load_name_full = [dir_results load_name];
+    % --- Now we will move the old (completed sessions) to the subject's folder.
+    index_all(index_savegame) = [];
 
-% --- Now we will move the old (completed sessions) to the subject's folder.
-index_all(index_savegame) = [];
-if nargin >= 3
-    % Moved only if dir_results_completed is specified
-    if ~isempty(index_all)
-        for j=1:length(index_all)
-            movefile([dir_results           ListSavegame(index_all(j)).name], ... % src
-                     [dir_results_completed ListSavegame(index_all(j)).name]);
+    if nargin >= 3
+        % Moved only if dir_results_completed is specified
+        if ~isempty(index_all)
+            for j=1:length(index_all)
+                movefile([dir_results           ListSavegame(index_all(j)).name], ... % src
+                         [dir_results_completed ListSavegame(index_all(j)).name]);
+            end
         end
     end
 end
