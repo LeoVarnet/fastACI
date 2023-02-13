aca6538eed7a79d90ff92619f888d7a48f2148a8
LeoVarnet <leo.varnet@ens.fr>
2023-02-13 13:01:46 +0100

    Create test_inclusion.m

---
+++ Publications/publ_carranante2023a_utils/test_inclusion.m
@@ -0,0 +1,16 @@
+function [SNRthres,is_included] = test_inclusion(expename, participantname)
+
+dir_data = fastACI_dir_data();
+dir_savegame = [dir_data expename '\' participantname '\Results\'];
+D = dir([dir_savegame 'savegame*.mat']);
+load([dir_savegame D(1).name])
+r = Get_mAFC_reversals(data_passation.expvar(1:400));
+SNRthres = median(r(5:end));
+is_included = (SNRthres<-12);
+fprintf(['Participant ' participantname ' obtained a SNR threshold of ' num2str(SNRthres) ' dB in the first session.\n'])
+if is_included
+    fprintf(['This participant can therefore be included in the experiment.\n'])
+else
+    fprintf(['This participant should therefore be rejected.\n'])
+end
+end
